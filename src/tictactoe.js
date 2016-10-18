8293587d0d39b5a05b8a4bdc1dd29b7556415e52
Chris Chiang <cchiang1992@gmail.com>
2016-10-18 12:49:13 -0700

    tictactoe command line in tictactoe.js

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,7 +1,14 @@
+var prompt = require('prompt');
+
 var board = [
   [[' '], [' '], [' ']],
   [[' '], [' '], [' ']],
   [[' '], [' '], [' ']]
-]
-
+];
 console.log(board);
+
+prompt.start();
+console.log('Choose a number between 1-9 for the square #, starting from the top left')
+prompt.get(['square'], function(err, res) {
+  console.log('response: ', res.square);
+});
