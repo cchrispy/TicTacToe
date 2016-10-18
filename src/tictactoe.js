d7bc7159cc8fd3707700a427860660ce4644d321
Chris Chiang <cchiang1992@gmail.com>
2016-10-18 13:28:44 -0700

    switch statement for toggling the square properly

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,15 +1,16 @@
 var prompt = require('prompt');
 
 var board = [
-  [[' '], ['X'], [' ']],
-  [[' '], ['X'], [' ']],
-  [[' '], ['X'], [' ']]
+  [[' '], [' '], [' ']],
+  [[' '], [' '], [' ']],
+  [[' '], [' '], [' ']]
 ];
 console.log(board);
 
 var complete = false;
 var filled = 0; // number of tic tac toe squares filled
-var checkCompletion = function() {
+var player = 'X';
+var checkCompletion = function() { // checks if the game is over
 
   board.forEach(function(row) { // check row
     if (row[0][0] === row[1][0] && row[1][0] === row[2][0]) {
@@ -23,13 +24,34 @@ var checkCompletion = function() {
     }
   }
 
-  
+  if (board[0][0][0] === board[1][1][0] && board[1][1][0] === board[2][2][0]
+   || board[0][2][0] === board[1][1][0] && board[1][1][0] === board[2][0][0]) { // check diagonals
+    complete = true;
+  }
 
 }
+
+var toggle = function(square) { // toggles the square according to the player
+  switch (square) {
+    case 'one': 
+    case 'two':
+    case 'three':
+    case 'four':
+    case 'five':
+    case 'six':
+    case 'seven':
+    case 'eight':
+    case 'nine':
+  }
+}
+
+
+
 checkCompletion();
 console.log(complete);
 
 prompt.start();
+
 console.log('Choose a number between 1-9 for the square #, starting from the top left')
 prompt.get(['square'], function(err, res) {
   console.log('response: ', res.square);
