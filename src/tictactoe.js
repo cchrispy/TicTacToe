a389df10cd515081038262b081ec2667340b40eb
Chris Chiang <cchiang1992@gmail.com>
2016-10-18 13:21:32 -0700

    checkCompletion helper function for rows and columns complete

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,11 +1,33 @@
 var prompt = require('prompt');
 
 var board = [
-  [[' '], [' '], [' ']],
-  [[' '], [' '], [' ']],
-  [[' '], [' '], [' ']]
+  [[' '], ['X'], [' ']],
+  [[' '], ['X'], [' ']],
+  [[' '], ['X'], [' ']]
 ];
 console.log(board);
+
+var complete = false;
+var filled = 0; // number of tic tac toe squares filled
+var checkCompletion = function() {
+
+  board.forEach(function(row) { // check row
+    if (row[0][0] === row[1][0] && row[1][0] === row[2][0]) {
+      complete = true;
+    }
+  });
+
+  for (var i = 0; i < 3; i++) { // check column
+    if (board[0][i][0] === board[1][i][0] && board[1][i][0] === board[2][i][0]) {
+      complete = true;
+    }
+  }
+
+  
+
+}
+checkCompletion();
+console.log(complete);
 
 prompt.start();
 console.log('Choose a number between 1-9 for the square #, starting from the top left')
