23f9724b6aa62015544e67d9c92166c54a9809c1
Chris Chiang <cchiang1992@gmail.com>
2016-10-18 12:50:40 -0700

    pseudocode complete

--- src/tictactoe.js
+++ src/tictactoe.js
@@ -12,3 +12,9 @@ console.log('Choose a number between 1-9 for the square #, starting from the top
 prompt.get(['square'], function(err, res) {
   console.log('response: ', res.square);
 });
+
+// have a function that checks if a game is complete or not
+// while the checking function is false, then prompt the user for a square number
+// toggle which player's turn it is (X or Y)
+// the respective square gets toggled as the X or Y in the board
+// when the checking function is true, determine the winner and the game ends
