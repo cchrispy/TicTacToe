d5d93bbe5e65c107fc32aace47326c0506dfbc65
Chris Chiang <cchiang1992@gmail.com>
2016-10-20 20:26:25 -0700

    tic tac toe now has marked squares with X and Os; win logic has not been implemented

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -17,55 +17,70 @@ class Game extends Component {
     };
   }
 
+  markSquare(square) {
+    this.setState({
+      [square]: this.props.name
+    })
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('one');
             this.props.changeUser(); 
           }} >
             { this.state.one }
           </div>
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('two');
             this.props.changeUser(); 
           }} >
             { this.state.two }
           </div>
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('three');
             this.props.changeUser(); 
           }} >
             { this.state.three }
           </div>
         </div>
         <div className="row">
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('four');
             this.props.changeUser(); 
           }} >
             { this.state.four }
           </div>
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('five');
             this.props.changeUser(); 
           }} >
             { this.state.five }
           </div>
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('six');
             this.props.changeUser(); 
           }} >
             { this.state.six }
           </div>
         </div>
         <div className="row">
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('seven');
             this.props.changeUser(); 
           }} >
             { this.state.seven }
           </div>
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('eight');
             this.props.changeUser(); 
           }} >
             { this.state.eight }
           </div>
-          <div className="col-xs-4" onClick={ () => {
+          <div className="col-xs-4 square" onClick={ () => {
+            this.markSquare('nine');
             this.props.changeUser(); 
           }} >
             { this.state.nine }
